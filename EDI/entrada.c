03983c26fc427d680622fdb240c12e956ed4094d
Pedro Henrique Holzmeier Schaefer <pedro.schaefer@estudante.uffs.edu.br>
2026-03-06 13:43:33 -0300

    aula 2 ed

---
+++ EDI/entrada.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argb[]){
+    int i;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &i);
+
+    printf("Voce digitou o numero: %d", i);
+    return 0;
+}
